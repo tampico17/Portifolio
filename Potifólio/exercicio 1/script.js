7723c63828da64dc3946470ea79987990348d91d
tampico17 <tamires.ramos.oliveira@escola.pr.gov.br>
2025-05-05 11:53:21 +0000

    continuacao

---
+++ Potifólio/exercicio 1/script.js
@@ -0,0 +1,17 @@
+function somar (){
+    let num1 = 0;
+    let num2 = 0;
+
+
+
+   
+    num1 = Number(window.prompt("digite um numero: "));
+    num2 = Number(window.prompt("digite um numero: "));
+
+let soma = num1 + num2;
+
+alert(soma)
+
+
+
+}
