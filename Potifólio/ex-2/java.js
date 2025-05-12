f2d2eb7d1dc9e8e41bb1262355b850afd784edc1
tampico17 <tamires.ramos.oliveira@escola.pr.gov.br>
2025-05-12 15:27:27 +0000

    www

--- Potifólio/ex-2/java.js
+++ Potifólio/ex-2/java.js
@@ -3,8 +3,23 @@ function maior(){
    let num1 = 0;
    const ano = 2025
    
- num1 = Number(window.prompt("digite um numero: "));
+num1 = Number(window.prompt("em que ano voce nasceu: "));
 
-let idade = num1 - ano
+let idade = ano - num1
+
+alert(idade)
+
+if(idade > 18){
+    alert("voce pode tirar a carteira de motorista");
+}
+if(idade == 18){
+    alert("voce pode tirar a carteira de motorista");
+}
+else{
+    let quant = 18 - idade
+alert("voce tem q esperar " + quant + " ano(s)")
+}
+
+
 
 }
