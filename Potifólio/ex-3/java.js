e35c64a00dda01db1993c14268586d4c26218d0f
tampico17 <tamires.ramos.oliveira@escola.pr.gov.br>
2025-05-16 12:45:25 +0000

    jjj

---
+++ Potifólio/ex-3/java.js
@@ -0,0 +1,22 @@
+function somar() {
+     num1 = Number(window.prompt(" "));
+     num2 = Number(window.prompt(" "));
+  
+num3 = num1 + num2
+}
+function dimi() {
+
+    num1 = Number(window.prompt(" "));
+    num2 = Number(window.prompt(" "));
+
+}
+function divi() {
+
+    num1 = Number(window.prompt(" "));
+    num2 = Number(window.prompt(" "));
+}
+function mult() {
+
+    num1 = Number(window.prompt(" "));
+    num2 = Number(window.prompt(" "));
+}
